d03752be80c039a6ba73ee8a4ce818ecb01f332e
Samson Githinji <githinjisamson148@gmail.com>
2023-11-21 07:48:21 +0300

    Completed assignment

---
+++ lib/app.py
@@ -0,0 +1,8 @@
+print("Hello World! Pass this test, please.")
+# print("Hello sun!")
+# print("Hello sky!")
+
+# print("Hello world!", end=" ")
+# print("Hello sun!", end="!! ")
+# print("Hello sky!", end="!!!\n")
+# print("hey")
