616b5e38caa85782fb49eebf7ac973bf27c801be
Samson <githinjisamson148@gmail.com>
2025-01-07 18:55:41 +0300

    Complete code

--- lib/app.py
+++ lib/app.py
@@ -1,8 +1,1 @@
 print("Hello World! Pass this test, please.")
-# print("Hello sun!")
-# print("Hello sky!")
-
-# print("Hello world!", end=" ")
-# print("Hello sun!", end="!! ")
-# print("Hello sky!", end="!!!\n")
-# print("hey")
